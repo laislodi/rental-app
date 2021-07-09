20e3ce6323318e3f7b7852f26e0be045b361b22c
Lais Gabrielle Lodi <laislodi@yahoo.com.br>
2021-07-08 22:52:38 -0700

    Use bootstrap to fix the Apartment page

--- src/public/units/ApartmentPage.tsx
+++ src/public/units/ApartmentPage.tsx
@@ -12,19 +12,43 @@ const ApartmentPage: React.FC = () => {
   if (!apartment) return <h1>NO APARTMENT...</h1>
 
   return (
-    <div>
-      <h1>Unidade {number}</h1>
-      <h2>{apartment.description}</h2>
-      {
-        apartment.images && apartment.images.length > 0
-        ? apartment.images.map((image) =>
-          <img key={image.id} src={image.url}
-               alt={'apartment-' + apartment.number} />
-          )
+    <div className="container py-4">
+      <h3>Unidade {number}</h3>
+      <div className="apartment-photo-album">
+        {
+          apartment.images ?
+            <div className='row'>
+              <div className='col-8'>
+                <img key={apartment.images[0].id} src={apartment.images[0].url}
+                     alt={'apartment-' + apartment.number}/>
+              </div>
+              <div className='col-4'>
+                <div className='row'>
+                  {
+                    apartment.images.map((image) => (
+                      <div className='col-6 p-1'>
+                        <img key={image.id} src={image.url}
+                             alt={'apartment-' + apartment.number}/>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
         : null
-      }
+        }
+        <div className='row'>
+          <div className="col-8 ">
+            <p className="p-2 apartment-description">{apartment.description}</p>
+          </div>
+          <div className='col-4'>
+            <h4 className='py-2'>Especificações</h4>
+            <p className="p-2">Here goes all of the specifications about the apartment</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default ApartmentPage
